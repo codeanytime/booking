37180305432aabe3aa9151698c15ae0f2a08b915
sonnth <sonnth@nal.vn>
2023-07-12 15:04:02 +0700

    add booking-app

---
+++ booking-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppMenu from './common/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <AppMenu />
+    </div>
+  );
+}
+
+export default App;
