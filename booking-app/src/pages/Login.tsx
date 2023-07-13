3e5263d94052087df1ae51dac3e5d5bbd4585d70
sonnth <sonnth@nal.vn>
2023-07-13 12:52:26 +0700

    add booking-app login

--- booking-app/src/pages/Login.tsx
+++ booking-app/src/pages/Login.tsx
@@ -6,119 +6,85 @@ import { login } from '../util/APIUtils';
 import { Link, redirect } from 'react-router-dom'
 import fbLogo from './img/fb-logo.png';
 import googleLogo from './img/google-logo.png';
-// import Alert from 'react-s-alert';
 
-class Login extends Component {
-    componentDidMount() {
-        // If the OAuth2 login encounters an error, the user is redirected to the /login page with an error.
-        // Here we display the error and then remove the error query parameter from the location.
-        if (this.props.location.state && this.props.location.state.error) {
-            setTimeout(() => {
-                Alert.error(this.props.location.state.error, {
-                    timeout: 5000
-                });
-                this.props.history.replace({
-                    pathname: this.props.location.pathname,
-                    state: {}
-                });
-            }, 100);
-        }
+export default function Login(props: any) {
+    if (props.authenticated) {
+        return (<div>Hello</div>);
     }
 
-    render() {
-        if (this.props.authenticated) {
-            return <Redirect
-                to={{
-                    pathname: "/",
-                    state: { from: this.props.location }
-                }} />;
-        }
-
-        return (
-            <div className="login-container">
-                <div className="login-content">
-                    <h1 className="login-title">Login to booking</h1>
-                    <SocialLogin />
-                    <div className="or-separator">
-                        <span className="or-text">OR</span>
-                    </div>
-                    <LoginForm {...this.props} />
-                    <span className="signup-link">New user? <Link to="/signup">Sign up!</Link></span>
+    return (
+        <div className="login-container">
+            <div className="login-content">
+                <h1 className="login-title">Login to booking</h1>
+                <SocialLogin />
+                <div className="or-separator">
+                    <span className="or-text">OR</span>
                 </div>
+                <LoginForm {...props} />
+                <span className="signup-link">New user? <Link to="/signup">Sign up!</Link></span>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-class SocialLogin extends Component {
-    render() {
-        return (
-            <div className="social-login">
-                <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
-                    <img src={googleLogo} alt="Google" /> Log in with Google</a>
-                <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
-                    <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a>
-            </div>
-        );
-    }
+const SocialLogin = () => {
+
+    return (
+        <div className="social-login">
+            <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
+                <img src={googleLogo} alt="Google" /> Log in with Google</a>
+            <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
+                <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a>
+        </div>
+    );
 }
 
 
-class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
 
-    handleInputChange(event) {
+const LoginForm = (props: any, state: any) => {
+    const [inputName, inputValue] = useState("");
+
+    const handleInputChange = (event: any) => {
         const target = event.target;
         const inputName = target.name;
         const inputValue = target.value;
 
-        this.setState({
-            [inputName]: inputValue
-        });
-    }
+        inputValue(inputValue);
+    };
 
-    handleSubmit(event) {
+    const handleSubmit = (event: any) => {
         event.preventDefault();
 
-        const loginRequest = Object.assign({}, this.state);
+        const loginRequest = Object.assign({}, state);
 
         login(loginRequest)
             .then(response => {
                 localStorage.setItem(ACCESS_TOKEN, response.accessToken);
-                Alert.success("You're successfully logged in!");
-                this.props.history.push("/");
+                props.history.push("/");
             }).catch(error => {
-                Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
+                // alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
             });
     }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-item">
-                    <input type="email" name="email"
-                        className="form-control" placeholder="Email"
-                        value={this.state.email} onChange={this.handleInputChange} required />
-                </div>
-                <div className="form-item">
-                    <input type="password" name="password"
-                        className="form-control" placeholder="Password"
-                        value={this.state.password} onChange={this.handleInputChange} required />
-                </div>
-                <div className="form-item">
-                    <button type="submit" className="btn btn-block btn-primary">Login</button>
-                </div>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-item">
+                <input type="email" name="email"
+                    className="form-control" placeholder="Email"
+                    value={state.email} onChange={handleInputChange} required />
+            </div>
+            <div className="form-item">
+                <input type="password" name="password"
+                    className="form-control" placeholder="Password"
+                    value={state.password} onChange={handleInputChange} required />
+            </div>
+            <div className="form-item">
+                <button type="submit" className="btn btn-block btn-primary">Login</button>
+            </div>
+        </form>
+    );
+}
+function state(arg0: {}, state: any) {
+    throw new Error('Function not implemented.');
 }
 
-export default Login
